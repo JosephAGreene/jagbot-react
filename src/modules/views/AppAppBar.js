7d513faceec6baf34bf195846fc25b3c9470736c
Joseph Greene <josephadamgreene@gmail.com>
2021-07-07 03:22:17 -0400

    Initial Commit

---
+++ src/modules/views/AppAppBar.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+import AppBar from '../components/AppBar';
+import Toolbar, { styles as toolbarStyles } from '../components/Toolbar';
+
+const styles = (theme) => ({
+  title: {
+    fontSize: 24,
+  },
+  titleLeft: {
+    color: theme.palette.common.white,
+  },
+  titleRight: {
+    color: theme.palette.secondary.main,
+  },
+  placeholder: toolbarStyles(theme).root,
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  left: {
+    flex: 1,
+  },
+  leftLinkActive: {
+    color: theme.palette.common.white,
+  },
+  right: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  rightLink: {
+    fontSize: 16,
+    color: theme.palette.common.white,
+    marginRight: theme.spacing(3),
+  },
+  linkSecondary: {
+    fontSize: 16,
+    color: theme.palette.secondary.main,
+  },
+});
+
+function AppAppBar(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          <div className={classes.left} />
+          <Link
+            variant="h6"
+            underline="none"
+            color="inherit"
+            className={classes.title}
+            href=""
+          >
+            <span className={classes.titleLeft}>Jag</span><span className={classes.titleRight}>Bot</span>
+          </Link>
+          <div className={classes.right}>
+            <Link
+              color="inherit"
+              variant="h6"
+              underline="none"
+              className={classes.rightLink}
+              href=""
+            >
+              {'Sign In'}
+            </Link>
+            <Link
+              variant="h6"
+              underline="none"
+              className={classes.linkSecondary}
+              href=""
+            >
+              {'Sign Up'}
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.placeholder} />
+    </div>
+  );
+}
+
+AppAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AppAppBar);
