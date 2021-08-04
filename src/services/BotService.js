3561e47f8d4d0cb150579a682f1517836f55eab6
Joseph Greene <josephadamgreene@gmail.com>
2021-08-04 18:08:35 -0400

    Add api calls BotService

--- src/services/BotService.js
+++ src/services/BotService.js
@@ -29,6 +29,28 @@ class BotService {
       }
     });
   }
+
+  deleteCommandModule(payload) {
+    return axios
+    .delete(`${BOT}command-module`, 
+      { 
+        data: payload
+      },
+      {
+        withCredentials: true
+      }
+    )
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      if (error.response) {
+        return error.response;
+      } else {
+        return this.networkIssue;
+      }
+    });
+  }
 }
 
 export default new BotService();
