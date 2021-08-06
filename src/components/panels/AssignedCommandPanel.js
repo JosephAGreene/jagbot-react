ec598125a32b2022ecf71f3dfae01f9f1f823d91
Joseph Greene <josephadamgreene@gmail.com>
2021-08-06 04:43:50 -0400

    delete functionality

--- src/components/panels/AssignedCommandPanel.js
+++ src/components/panels/AssignedCommandPanel.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+// Import API service
+import BotService from "../../services/BotService.js";
+
 // Import MUI components
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 // Import custom components
 import GridContainer from '../grid/GridContainer';
@@ -11,11 +16,23 @@ import GridItem from '../grid/GridItem';
 import Button from '../buttons/Button';
 
 const styles = (theme) => ({
+  menuRoot: {
+    '& .MuiPaper-root': {
+      backgroundColor: theme.palette.gray.dark,
+      borderRadius: 2,
+    },
+    '& .MuiButtonBase-root': {
+      color: theme.palette.white.dark,
+      '&:hover' : {
+        backgroundColor: theme.palette.gray.light,
+      },
+      
+    },
+  },
   paper: {
     padding: "20px",
     marginBottom: theme.spacing(3),
     backgroundColor: theme.palette.gray.main,
-    overflow: "hidden",
   },
   command: {
     fontSize: 18,
@@ -32,32 +49,86 @@ const styles = (theme) => ({
 });
 
 function AssignedCommandPanel(props) {
-  const {classes, command, description} = props;
+  const {classes, bots, botId, moduleId, command, description, setBots, setApiAlert} = props;
+
+  const [deleteAnchor, setDeleteAnchor] = React.useState(null);
+
+  const handleDeleteClick = (event) => {
+    setDeleteAnchor(event.currentTarget);
+  };
+
+  const handleDeleteClose = () => {
+    setDeleteAnchor(null);
+  };
+
+  const handleDeleteConfirm = async () => {
+    const payload = {
+      "botId": botId,
+      "moduleId": moduleId,
+    }
+
+    const res = await BotService.deleteCommandModule(payload);
+
+    if (res.status === 200) {
+      let newBots = [...bots];
+      for (let i=0; i < bots.length; i++) {
+        if(bots[i]._id === botId) {
+          newBots[i] = res.data;
+          break;
+        }
+      }
+
+      setBots(newBots);
+      setApiAlert({
+        status: true,
+        duration: 3000,
+        severity: "success",
+        message: "Module deleted!"
+      });
+    }
+  }
 
   return (
     <GridContainer>
-      <GridItem sm={12}>
+      <GridItem xs>
         <Paper elevation={2} className={classes.paper} >
           <GridContainer>
-            <GridItem sm={12} md={10} lg={8}>
-              <GridContainer>
-                <GridItem xs>
-                  <div className={classes.command}>
-                    {command}
-                  </div>
-                  <div className={classes.description}>
-                    {description}
-                  </div>
-                </GridItem>
-              </GridContainer>
+            <GridItem xs={12} sm={12} md={8} lg={8}>
+              <div className={classes.command}>
+                {command}
+              </div>
+              <div className={classes.description}>
+                {description}
+              </div>
             </GridItem>
-            <GridItem xs right>
+            <GridItem xs={12} sm={12} md={4} lg={4} right>
               <div className={classes.buttons}>
                 <Button color="purple">Edit</Button>
               </div>
               <div className={classes.buttons}>
-                <Button color="danger">Delete</Button>
+                <Button onClick={handleDeleteClick} color="danger">Delete</Button>
               </div>
+              <Menu
+                id="delete-menu"
+                anchorEl={deleteAnchor}
+                open={Boolean(deleteAnchor)}
+                onClose={handleDeleteClose}
+                className={classes.menuRoot}
+                getContentAnchorEl={null}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+              >
+                <MenuItem onClick={handleDeleteConfirm}>
+                    Confirm
+                </MenuItem>
+                <MenuItem onClick={handleDeleteClose}>Cancel</MenuItem>
+              </Menu>
             </GridItem>
           </GridContainer>
         </Paper>
