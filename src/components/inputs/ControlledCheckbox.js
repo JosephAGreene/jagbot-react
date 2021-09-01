cc1e34f4ba47f5d10a8933913e4c8baf6e60b9ac
Joseph Greene <josephadamgreene@gmail.com>
2021-09-01 06:55:45 -0400

    Add custom controlled checkbox input

---
+++ src/components/inputs/ControlledCheckbox.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+// Import react-hook-form components
+import { Controller } from 'react-hook-form';
+
+// Import Mui components
+import { withStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+const styles = (theme) => ({
+  labelRootError: {
+    color: theme.palette.error.main,
+    marginLeft: "15px",
+  },
+});
+
+const CustomCheckbox =  withStyles((theme) =>({
+  root: {
+    color: theme.palette.teal.dark,
+    '&$checked': {
+      color: theme.palette.teal.main,
+    },
+  },
+  checked: {},
+}))((props) => <Checkbox color="default" {...props} />);
+
+function ControlledCheckbox(props) {
+  const { classes, control, name, error, label, labelPlacement } = props;
+
+  return (
+    <React.Fragment>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <FormControlLabel
+            control={
+              <CustomCheckbox
+                onChange={(e) => field.onChange(e.target.checked)}
+                checked={field.value}
+                disableRipple="true"
+              />
+            }
+            label={label}
+            labelPlacement={labelPlacement ? labelPlacement : "end"}
+          />
+        )}
+      />
+      {error[name] ?
+        <FormHelperText className={classes.labelRootError} id={`error-message-${name}`}>{error[name].message}</FormHelperText>
+        : <FormHelperText> </FormHelperText>
+      }
+    </React.Fragment>
+  );
+}
+
+ControlledCheckbox.propTypes = {
+  classes: PropTypes.object.isRequired,
+  control: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  error: PropTypes.object,
+  label: PropTypes.string,
+  labelPlacement: PropTypes.string,
+};
+
+export default withStyles(styles)(ControlledCheckbox);
