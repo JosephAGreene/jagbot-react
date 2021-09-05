845a1bfe2444c7e814aab5ef3ff979752dab01d0
Joseph Greene <josephadamgreene@gmail.com>
2021-09-05 06:30:16 -0400

    Add BotService api service

---
+++ src/services/BotService.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { BOT } from "./apiVariables.js";
+
+class BotService {
+
+  constructor() {
+    // Generic response to give API experiences internal issues.
+    this.networkIssue = {status: 'dead'};
+  }
+
+  getBot(id) {
+    return axios
+    .get(`${BOT}/${id}`, 
+      {
+        withCredentials: true
+      }
+    )
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      if (error.response) {
+        return error.response;
+      } else {
+        return this.networkIssue;
+      }
+    });
+  }
+
+  getServerRoles(payload) {
+    return axios
+    .post(`${BOT}/server-roles`, 
+      {
+        ...payload
+      },
+      {
+        withCredentials: true
+      }
+    )
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      if (error.response) {
+        return error.response;
+      } else {
+        return this.networkIssue;
+      }
+    });
+  }
+
+  updateInviteFilter(payload) {
+    return axios
+    .post(`${BOT}/invite-filter`, 
+      {
+        ...payload
+      },
+      {
+        withCredentials: true
+      }
+    )
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      if (error.response) {
+        return error.response;
+      } else {
+        return this.networkIssue;
+      }
+    });
+  }
+
+
+}
+
+export default new BotService();
