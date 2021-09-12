da183cdefdc1663c10c24ead17cb907775c0085e
Joseph Greene <josephadamgreene@gmail.com>
2021-09-12 05:21:02 -0400

    Embed response type functionality

--- src/views/modules/CustomCommandSingle.js
+++ src/views/modules/CustomCommandSingle.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import {useHistory, useLocation} from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 // Import API service
 import CustomModuleService from "../../services/CustomModuleService.js";
 
 // Import react-hook-form
-import { useForm } from 'react-hook-form';
+import { useForm, useFieldArray } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
 import Joi from 'joi';
 
@@ -21,11 +21,13 @@ import Paper from '@material-ui/core/Paper';
 import TitlePanel from '../panels/TitlePanel';
 import OutlinedInput from '../../components/inputs/OutlinedInputDark';
 import ResponseEditor from '../../components/inputs/ResponseEditor';
+import EmbedEditor from './editors/EmbedEditor';
 import Button from '../../components/buttons/Button';
 import GridContainer from '../../components/grid/GridContainer';
 import GridItem from '../../components/grid/GridItem';
 import ControlledRadioGroup from '../../components/inputs/ControlledRadioGroup';
 import ControlledRadio from '../../components/inputs/ControlledRadio';
+
 
 // Import icons
 import { TiMessage } from 'react-icons/ti';
@@ -50,7 +52,7 @@ const styles = (theme) => ({
   },
   edit: {
     color: theme.palette.purple.main,
-  }
+  },
 });
 
 const schema = Joi.object({
@@ -64,13 +66,13 @@ const schema = Joi.object({
     })
     .messages({
       "string.empty": 'Command is required',
-      "string.max" : 'Command cannot be greater than 30 characters',
+      "string.max": 'Command cannot be greater than 30 characters',
       "any.required": 'Command is required',
     }),
   description: Joi.string().trim().max(250).required()
     .messages({
       "string.empty": 'Description is required',
-      "string.max" : 'Description cannot be greater than 250 characters',
+      "string.max": 'Description cannot be greater than 250 characters',
       "any.required": 'Description is required',
     }),
   responseLocation: Joi.string().trim().required()
@@ -78,13 +80,135 @@ const schema = Joi.object({
       "string.empty": 'Response Location is required',
       "any.required": 'Response Location is required',
     }),
-  response: Joi.string().trim().max(2000).required()
+  responseType: Joi.string().trim().required()
+    .messages({
+      "string.empty": 'Response Type is required',
+      "any.required": 'Response Type is required',
+    }),
+  response: Joi.when('responseType', {
+    is: Joi.string().trim().valid("basic"),
+    then: Joi.string().trim().max(1500).required(),
+    otherwise: Joi.string().allow('').default('').trim().optional().max(1500)
+  })
     .messages({
       "string.empty": 'Response is required',
-      "string.max" : 'Response cannot be greater than 2000 characters',
+      "string.max": 'Response cannot be greater than 1500 characters',
       "any.required": 'Response is required',
     }),
+  embedTitle: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.string().trim().max(240).required(),
+    otherwise: Joi.string().trim().max(240).allow('').optional(),
+  })
+    .messages({
+      "string.empty": 'Title is required',
+      "string.max": "Title cannot be greater than 240 characters",
+      "any.required": 'Title is required',
+    }),
+  embedLinkURL: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.string().trim().max(2040).allow('').optional(),
+    otherwise: Joi.string().trim().max(2040).allow('').optional(),
+  })
+    .messages({
+      "string.max": "Urls cannot be greater than 2040 characters",
+    }),
+  embedColor: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.string().trim().regex(RegExp(/^#([0-9a-f]{3}|[0-9a-f]{6})$/i)).max(7).allow('').optional(),
+    otherwise: Joi.string().trim().regex(RegExp(/^#([0-9a-f]{3}|[0-9a-f]{6})$/i)).max(7).allow('').optional(),
+  })
+    .messages({
+      "string.pattern.base": "Color must be a valid hex code",
+      "string.max": "Color must be a valid hex code",
+    }),
+  embedThumbnailURL: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.string().trim().max(2040).allow('').optional(),
+    otherwise: Joi.string().trim().max(2040).allow('').optional(),
+  })
+    .messages({
+      "string.max": "Urls cannot be greater than 2040 characters",
+    }),
+  embedMainImageURL: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.string().trim().max(2040).allow('').optional(),
+    otherwise: Joi.string().trim().max(2040).allow('').optional(),
+  })
+    .messages({
+      "string.max": "Urls cannot be greater than 2040 characters",
+    }),
+  embedDescription: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.string().trim().max(3000).allow('').optional(),
+    otherwise: Joi.string().trim().max(3000).allow('').optional(),
+  })
+    .messages({
+      "string.max": "Description cannot be greater than 3000 characters",
+    }),
+  embedFields: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.array().items(Joi.object({
+      name: Joi.string().trim().max(240).required()
+        .messages({
+          "string.empty": "Name is required",
+          "string.max": "Name cannot be greater than 240 characters",
+        }),
+      value: Joi.string().trim().max(750).required()
+        .messages({
+          "string.empty": "Value is required",
+          "string.max": "Value cannot be greater than 750 characters"
+        }),
+      inline: Joi.bool().required(),
+      _id: Joi.string().allow('').default('').trim().optional(), // Ignore _id field
+    })).required(),
+    otherwise: Joi.array().items(Joi.object({
+      name: Joi.string().trim().max(240).allow('').optional()
+        .messages({
+          "string.max": "Name cannot be greater than 240 characters",
+        }),
+      value: Joi.string().trim().max(750).allow('').optional()
+        .messages({
+          "string.max": "Value cannot be greater than 750 characters"
+        }),
+      inline: Joi.bool().default(false).optional(),
+      _id: Joi.string().allow('').default('').trim().optional(), // Ignore _id field
+    })).optional(),
+  }),
+  embedFooter: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.string().trim().max(500).allow('').optional(),
+    otherwise: Joi.string().trim().max(500).allow('').optional(),
+  })
+    .messages({
+      "string.max": "Footer cannot be greater than 3000 characters",
+    }),
+  embedFooterThumbnailURL: Joi.when('responseType', {
+    is: Joi.string().trim().valid("embed"),
+    then: Joi.string().trim().max(2040).allow('').optional(),
+    otherwise: Joi.string().trim().max(2040).allow('').optional(),
+  })
+    .messages({
+      "string.max": "Urls cannot be greater than 2040 characters",
+    }),
 });
+
+// Returns true is max char count of embed fields
+// is greater than 5,500 characters
+function validMaxCharCount (data) {
+  let count = 0;
+
+  count += data.embedTitle.trim().length;
+  count += data.embedDescription.trim().length;
+  count += data.embedFooter.trim().length;
+  data.embedFields.forEach((field) => {
+    count += field.name.trim().length;
+    count += field.value.trim().length;
+  })
+
+  if(count > 5500) {return true;}
+  return false;
+}
 
 function setDefaultValues(module) {
   if (module) {
@@ -92,31 +216,66 @@ function setDefaultValues(module) {
       command: module.command,
       description: module.description,
       responseLocation: module.responseLocation,
+      responseType: module.responseType,
       response: module.response,
+      embedTitle: module.embedTitle,
+      embedLinkURL: module.LinkURL,
+      embedColor: module.embedColor,
+      embedThumbnailURL: module.embedThumbnailURL,
+      embedMainImageURL: module.embedMainImageURL,
+      embedDescription: module.embedDescription,
+      embedFields: module.embedFields,
+      embedFooter: module.embedFooter,
+      embedFooterThumbnailURL: module.embedFooterThumbnailURL,
     }
   } else {
     return {
       command: '',
       description: '',
       responseLocation: 'server',
+      responseType: "basic",
       response: '',
+      embedTitle: "",
+      embedLinkURL: "",
+      embedColor: "#ffffff",
+      embedThumbnailURL: "",
+      embedMainImageURL: "",
+      embedDescription: "",
+      embedFields: [],
+      embedFooter: "",
+      embedFooterThumbnailURL: "",
     }
   }
-} 
+}
 
 function CustomCommandSingle(props) {
-  const {classes, selectedBot, setSelectedBot, setApiAlert} = props;
-  const {module} = useLocation();
+  const { classes, selectedBot, setSelectedBot, setApiAlert } = props;
+  const { module } = useLocation();
 
-  const {register, handleSubmit, control, watch, setValue, setError, formState:{errors}} = useForm({
+  const { register, handleSubmit, control, watch, setValue, setError, trigger, formState: { errors } } = useForm({
     resolver: joiResolver(schema),
     defaultValues: setDefaultValues(module),
   });
 
+  const { fields, append, swap, remove } = useFieldArray({ control, name: "embedFields" });
+
   const watchResponse = watch("response", (module ? module.response : ''));
+  const watchEmbedDescription = watch("embedDescription", (module ? module.embedDescription : ''));
+  const watchResponseType = watch("responseType");
+  const watchEmbedColor = watch("embedColor");
   const history = useHistory();
 
   const onSubmit = async (data) => {
+    if(validMaxCharCount(data)) {
+      setApiAlert({
+        status: true,
+        duration: 6000,
+        severity: "error",
+        message: "The combined character count of embed title, description, fields, and footer cannot exceed 5,500!"
+      });
+      return;
+    } 
+
     if (module) {
       submitUpdateModule(data);
     } else {
@@ -141,10 +300,10 @@ function CustomCommandSingle(props) {
         message: "A new single-response command has been added!"
       });
       history.push('/dashboard/develop/customcommands');
-    } 
-    
+    }
+
     if (res.status === 409 && res.data === "duplicate command") {
-      setError("command", {type: "manual", message: "Command trigger word already exists."});
+      setError("command", { type: "manual", message: "Command trigger word already exists." });
     }
   }
 
@@ -173,9 +332,45 @@ function CustomCommandSingle(props) {
     history.push('/dashboard/develop/customcommands');
   }
 
+  const returnResponseEditor = () => {
+    if (watchResponseType === "basic") {
+      return (
+        <ResponseEditor
+          labelText="Response"
+          description="The response your bot will give."
+          id="response"
+          name="response"
+          watch={watchResponse}
+          setValue={setValue}
+          maxLength={1500}
+          multiline
+          rows={10}
+          formControlProps={{ fullWidth: true }}
+          inputProps={{ ...register("response"), maxLength: 1500 }}
+          error={errors}
+        />
+      )
+    }
+    return (
+      <EmbedEditor
+        fields={fields}
+        control={control}
+        register={register}
+        watchEmbedDescription={watchEmbedDescription}
+        watchEmbedColor={watchEmbedColor}
+        setValue={setValue}
+        trigger={trigger}
+        errors={errors}
+        append={append}
+        swap={swap}
+        remove={remove}
+      />
+    );
+  }
+
   return (
     <ContentWrapper>
-      <TitlePanel 
+      <TitlePanel
         title="Single Response"
         description="A single command that returns a single response"
         Icon={TiMessage}
@@ -187,13 +382,13 @@ function CustomCommandSingle(props) {
       </div>
       <Paper className={classes.paper}>
         <form autoComplete="off" onSubmit={handleSubmit(onSubmit)} >
-        <OutlinedInput
+          <OutlinedInput
             labelText="Command"
             description="Command Trigger Word"
             id="command"
             name="command"
-            formControlProps={{fullWidth: true}}
-            inputProps={{...register("command"), maxLength: 30}}
+            formControlProps={{ fullWidth: true }}
+            inputProps={{ ...register("command"), maxLength: 30 }}
             error={errors}
           />
           <OutlinedInput
@@ -201,12 +396,12 @@ function CustomCommandSingle(props) {
             description="Short description of the command."
             id="description"
             name="description"
-            formControlProps={{fullWidth: true}}
-            inputProps={{...register("description"), maxLength: 250}}
+            formControlProps={{ fullWidth: true }}
+            inputProps={{ ...register("description"), maxLength: 250 }}
             error={errors}
           />
-          <ControlledRadioGroup 
-            control={control} 
+          <ControlledRadioGroup
+            control={control}
             name="responseLocation"
             description="Where should the bot respond?"
             defaultValue="server"
@@ -221,20 +416,23 @@ function CustomCommandSingle(props) {
               label="Direct Message the User"
             />
           </ControlledRadioGroup>
-          <ResponseEditor
-            labelText="Response"
-            description="The response your bot will give."
-            id="response"
-            name="response"
-            watch={watchResponse}
-            setValue={setValue}
-            maxLength={2000}
-            multiline
-            rows={10}
-            formControlProps={{fullWidth: true}}
-            inputProps={{...register("response"), maxLength: 2000}}
+          <ControlledRadioGroup
+            control={control}
+            name="responseType"
+            description="Response Type"
+            defaultValue="basic"
             error={errors}
-          />
+          >
+            <ControlledRadio
+              value="basic"
+              label="Basic"
+            />
+            <ControlledRadio
+              value="embed"
+              label="Embed"
+            />
+          </ControlledRadioGroup>
+          {returnResponseEditor()}
           <GridContainer justifyContent="flex-end">
             <GridItem>
               <Button
