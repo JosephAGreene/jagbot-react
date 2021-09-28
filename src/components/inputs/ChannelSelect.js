4a6ac700c7cb39dc8524e23372dd3e2ade155402
Joseph Greene <josephadamgreene@gmail.com>
2021-09-27 23:50:57 -0400

    custom ChannelSelect autocomplete component

---
+++ src/components/inputs/ChannelSelect.js
@@ -0,0 +1,227 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+// Import react-hook-form
+import { Controller } from "react-hook-form";
+
+// Import API service
+import BotService from "../../services/BotService.js";
+
+// @material-ui/core components
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from '@material-ui/core/FormHelperText';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+// Import icons
+import SyncIcon from '@material-ui/icons/Sync';
+
+const styles = (theme) => ({
+  autocompleteRoot: {
+    '& .MuiIconButton-root': {
+      color: theme.palette.white.main,
+    }
+  },
+  textFieldRoot: {
+    '& .MuiFormLabel-root': {
+      color: theme.palette.white.dark,
+    },
+    '& label.Mui-focused': {
+      color: theme.palette.white.main,
+    },
+    '& label.Mui-error': {
+      color: theme.palette.error.main,
+    },
+    '& .MuiOutlinedInput-root': {
+      backgroundColor: theme.palette.gray.main,
+      color: theme.palette.white.main,
+      "& .MuiOutlinedInput-notchedOutline": {
+        borderColor: theme.palette.teal.dark,
+      },
+      '&:hover .MuiOutlinedInput-notchedOutline': {
+        borderColor: theme.palette.teal.light,
+      },
+      "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+        borderColor: theme.palette.teal.main
+      },
+      "&.Mui-error .MuiOutlinedInput-notchedOutline": {
+        borderColor: theme.palette.error.main
+      }
+    },
+  },
+  dropdownPaper: {
+    backgroundColor: theme.palette.gray.dark,
+    boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)',
+  },
+  dropdownLabel: {
+    color: theme.palette.white.main,
+    backgroundColor: theme.palette.gray.dark,
+  },
+  dropdownOption: {
+    color: theme.palette.white.dark,
+    '&:hover': {
+      backgroundColor: theme.palette.gray.light,
+    }
+  },
+  dropdownUl: {
+    backgroundColor: theme.palette.gray.dark,
+  },
+  dropdownNoOptions: {
+    color: theme.palette.white.dark,
+  },
+  labelRootError: {
+    color: theme.palette.error.main
+  },
+  formControl: {
+    paddingBottom: "5px",
+    margin: "5px 0 0 0",
+    position: "relative",
+    verticalAlign: "unset",
+    width: "100%",
+  },
+  description: {
+    color: theme.palette.white.dark,
+    margin: "0 0 10px 0",
+    fontSize: "16px",
+  },
+  syncButton: {
+    padding: 0,
+    margin: 0,
+  },
+  syncIcon: {
+    color: theme.palette.teal.dark,
+    '&:hover': {
+      color: theme.palette.teal.light,
+    },
+  },
+  progress: {
+    color: theme.palette.teal.light,
+    padding: 0,
+    marginLeft: "2px",
+    marginRight: "2px",
+  }
+});
+
+function ChannelSelect(props) {
+  const { 
+    classes, 
+    control, 
+    name, 
+    label, 
+    description, 
+    formControlProps, 
+    error, 
+    selectedBot, 
+    setApiAlert 
+  } = props;
+
+  const [loading, setLoading] = React.useState(false);
+  const [channels, setChannels] = React.useState(selectedBot.serverChannels ? selectedBot.serverChannels : []);
+
+  const handleBotChannelsSync = async () => {
+    setLoading(true);
+
+    const payload = {
+      botId: selectedBot._id,
+    };
+
+    const res = await BotService.getBotChannels(payload);
+
+    if(res.status === 200) {
+      setChannels(res.data);
+    }
+
+    if(res.status === 'dead') {
+      setApiAlert({
+        status: true,
+        duration: 4000,
+        severity: "error",
+        message: "Server is down or busy!"
+      });
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <FormControl
+      {...formControlProps}
+      className={classes.formControl}
+      variant="outlined"
+    >
+      <div className={classes.description}>
+        {description}
+      </div>
+      <Controller
+        control={control}
+        name="channel"
+        render={({ field: { onChange, value } }) => (
+          <Autocomplete
+            className={classes.autocompleteRoot}
+            classes={{
+              paper: classes.dropdownPaper,
+              groupLabel: classes.dropdownLabel,
+              groupUl: classes.dropdownUl,
+              option: classes.dropdownOption,
+              noOptions: classes.dropdownNoOptions,
+            }}
+            onChange={(event, option) => {
+              onChange(option);
+            }}
+            value={value}
+            options={loading ? [] : channels.sort((a, b) => -b.serverName.localeCompare(a.serverName))}
+            groupBy={(option) => `Server: ${option.serverName}`}
+            renderOption={(option) => (option.channelName ? option.channelName : "")}
+            getOptionLabel={(option) => (option.channelName ? `${option.serverName}: ${option.channelName}` : "")}
+            getOptionSelected={option => option}
+            noOptionsText={loading ?  "Fetching channels..." : "No channels found. Try refreshing."}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                className={classes.textFieldRoot}
+                label={label}
+                variant="outlined"
+                InputProps={{
+                  ...params.InputProps,
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      {loading
+                        ? <CircularProgress className={classes.progress} style={{ height: "20px", width: "20px" }} />
+                        : <IconButton className={classes.syncButton} aria-label="sync channels" onClick={handleBotChannelsSync} >
+                            <SyncIcon className={classes.syncIcon} />
+                          </IconButton>
+                      }
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            )}
+          />
+
+        )}
+      />
+      {error[name] ?
+        <FormHelperText className={classes.labelRootError} id={`error-message-${name}`}>{error[name].message}</FormHelperText>
+        : <FormHelperText> </FormHelperText>
+      }
+    </FormControl>
+  );
+}
+
+ChannelSelect.propTypes = {
+  classes: PropTypes.object.isRequired,
+  control: PropTypes.object.isRequired,
+  name: PropTypes.string,
+  label: PropTypes.string,
+  description: PropTypes.string,
+  formControlProps: PropTypes.object,
+  error: PropTypes.object,
+  selectedBot: PropTypes.object.isRequired,
+  setApiAlert: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(ChannelSelect);
--- src/views/modules/AnnouncementsJoin.js
+++ src/views/modules/AnnouncementsJoin.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+// Import react-hook-form
+import { useForm, useFieldArray, useWatch } from 'react-hook-form';
 
 // Import layouts
 import ContentWrapper from '../../layouts/ContentWrapper';
 
 // Import MUI components
 import { withStyles } from '@material-ui/core/styles';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Paper from '@material-ui/core/Paper';
 
 // Import custom components
 import TitlePanel from '../panels/TitlePanel';
+import GridContainer from '../../components/grid/GridContainer';
+import GridItem from '../../components/grid/GridItem';
+import Button from '../../components/buttons/Button';
+import ResponseEditor from '../../components/inputs/ResponseEditor';
+import EmbedEditor from './editors/EmbedEditor';
+import ControlledRadioGroup from '../../components/inputs/ControlledRadioGroup';
+import ControlledRadio from '../../components/inputs/ControlledRadio';
+import ChannelSelect from '../../components/inputs/ChannelSelect';
 
 // Import Icons
 import { FaUserPlus } from "react-icons/fa";
 
 const styles = (theme) => ({
+  paper: {
+    padding: "20px",
+    marginBottom: theme.spacing(3),
+    backgroundColor: theme.palette.gray.main,
+    overflow: "hidden",
+    color: theme.palette.white.main,
+  },
   categoryHeader: {
     marginLeft: theme.spacing(1),
     color: theme.palette.white.dark,
@@ -26,8 +47,115 @@ const styles = (theme) => ({
   },
 });
 
+function setDefaultValues(module) {
+  // if (module) {
+  //   return {
+  //     command: module.command,
+  //     description: module.description,
+  //     responseLocation: module.responseLocation,
+  //     responseType: module.responseType,
+  //     response: module.response,
+  //     embedTitle: module.embedTitle,
+  //     embedLinkURL: module.embedLinkURL,
+  //     embedColor: module.embedColor,
+  //     embedThumbnailURL: module.embedThumbnailURL,
+  //     embedMainImageURL: module.embedMainImageURL,
+  //     embedDescription: module.embedDescription,
+  //     embedFields: module.embedFields,
+  //     embedFooter: module.embedFooter,
+  //     embedFooterThumbnailURL: module.embedFooterThumbnailURL,
+  //   }
+  // } else {
+    return {
+      channel: "",
+      responseType: "basic",
+      response: '',
+      embedTitle: "",
+      embedLinkURL: "",
+      embedColor: "#ffffff",
+      embedThumbnailURL: "",
+      embedMainImageURL: "",
+      embedDescription: "",
+      embedFields: [],
+      embedFooter: "",
+      embedFooterThumbnailURL: "",
+    }
+  // }
+}
+
 
 function AnnouncementsJoin(props) {
+  const { classes, selectedBot, setApiAlert } = props;
+  const history = useHistory();
+
+  const { register, handleSubmit, control, watch, setValue, trigger, formState: { errors } } = useForm({
+    // resolver: joiResolver(schema),
+     defaultValues: setDefaultValues(module),
+  });
+
+  const { fields, append, swap, remove } = useFieldArray({ control, name: "embedFields" });
+  const watchFields = useWatch({ control, name: "embedFields" }, fields);
+  const watchResponse = watch("response", (module ? module.response : ''));
+  const watchEmbedDescription = watch("embedDescription", (module ? module.embedDescription : ''));
+  const watchResponseType = watch("responseType");
+  const watchEmbedColor = watch("embedColor");
+
+  const embedObject = {
+    title: watch("embedTitle"),
+    linkURL: watch("embedLinkURL"),
+    color: watchEmbedColor,
+    thumbnailURL: watch('embedThumbnailURL'),
+    mainImageURL: watch('embedMainImageURL'),
+    description: watchEmbedDescription,
+    fields: watchFields,
+    footer: watch('embedFooter'),
+    footerThumbnailURL: watch('embedFooterThumbnailURL'),
+  }
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
+  const handleCancel = () => {
+    history.push('/dashboard/develop/announcements');
+  }
+
+  const returnResponseEditor = () => {
+    if (watchResponseType === "basic") {
+      return (
+        <ResponseEditor
+          labelText="Announcement"
+          description="The announcement your bot will give."
+          id="response"
+          name="response"
+          watch={watchResponse}
+          setValue={setValue}
+          maxLength={1500}
+          multiline
+          rows={10}
+          formControlProps={{ fullWidth: true }}
+          inputProps={{ ...register("response"), maxLength: 1500 }}
+          error={errors}
+        />
+      )
+    }
+    return (
+      <EmbedEditor
+        fields={fields}
+        control={control}
+        register={register}
+        watchEmbedDescription={watchEmbedDescription}
+        watchEmbedColor={watchEmbedColor}
+        embedObject={embedObject}
+        setValue={setValue}
+        trigger={trigger}
+        errors={errors}
+        append={append}
+        swap={swap}
+        remove={remove}
+      />
+    );
+  }
 
   return (
     <ContentWrapper>
@@ -38,6 +166,62 @@ function AnnouncementsJoin(props) {
         color="#98c379"
         docs={true}
       />
+      <Paper className={classes.paper}>
+        <form autoComplete="off" onSubmit={handleSubmit(onSubmit)} >
+          <ChannelSelect 
+            selectedBot={selectedBot}
+            setApiAlert={setApiAlert}
+            control={control}
+            name="channel"
+            label="Channel"
+            description="Channel to post announcement"
+            error={errors}
+          />
+          <ControlledRadioGroup
+            control={control}
+            name="responseType"
+            description="Announcement Type"
+            defaultValue="basic"
+            error={errors}
+          >
+            <ControlledRadio
+              value="basic"
+              label="Basic"
+            />
+            <ControlledRadio
+              value="embed"
+              label="Embed"
+            />
+          </ControlledRadioGroup>
+          {returnResponseEditor()}
+          {errors.maxChar
+            ? <FormHelperText className={classes.labelRootError} id={`error-message-maxChar`}>
+              The combined character count of embed title, description, fields, and footer cannot exceed 5,500!
+            </FormHelperText>
+            : <FormHelperText> </FormHelperText>
+          }
+          <GridContainer justifyContent="flex-end">
+            <GridItem>
+              <Button
+                onClick={handleCancel}
+                variant="contained"
+                color="danger"
+              >
+                Cancel
+              </Button>
+            </GridItem>
+            <GridItem>
+              <Button
+                type="submit"
+                variant="contained"
+                color="teal"
+              >
+                Save
+              </Button>
+            </GridItem>
+          </GridContainer>
+        </form>
+      </Paper>
 
     </ContentWrapper>
   );
