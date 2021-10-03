64289e3042064305fa29a4326d0da2f3e7c36607
Joseph Greene <josephadamgreene@gmail.com>
2021-10-03 19:27:42 -0400

    add listItems prop / better responsiveness

--- src/views/panels/TitlePanel.js
+++ src/views/panels/TitlePanel.js
@@ -3,8 +3,13 @@ import PropTypes from "prop-types";
 
 // Import MUI components
 import { withStyles } from '@material-ui/core/styles';
+import Hidden from '@material-ui/core/Hidden';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 // Import custom components
 import GridContainer from '../../components/grid/GridContainer';
@@ -13,6 +18,7 @@ import Button from '../../components/buttons/Button';
 
 // Import icons
 import { SiReadthedocs } from 'react-icons/si';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 
 const styles = (theme) => ({
   paper: {
@@ -30,59 +36,126 @@ const styles = (theme) => ({
   title: {
     color: theme.palette.white.main,
     fontSize: 28,
+    width: "100%",
   },
   description: {
     color: theme.palette.white.dark,
   },
-  button: {
+  list: {
     marginTop: theme.spacing(1),
-    marginLeft: theme.spacing(1),
+    '& .MuiListItemText-root': {
+      padding: 0,
+    },
+    '& .MuiListItem-gutters': {
+      padding: 0,
+    },
+    '& .MuiListItemIcon-root': {
+      minWidth: "unset !important",
+      color: theme.palette.error.light,
+    },
+  },
+  documentButton: {
+    color: theme.palette.teal.main,
+    '& .MuiIconButton-root': {
+      padding: "0px",
+    }
   },
 });
 
 function TitlePanel(props) {
-  const {classes, title, description, Icon, image, docs, color} = props;
+  const { classes, title, description, listItems, Icon, image, docs, color } = props;
 
   return (
     <GridContainer>
       <GridItem sm={12}>
         <Paper elevation={2} className={classes.paper} >
-          <GridContainer>
-            <GridItem sm={12} md={10} lg={8}>
-              <GridContainer>
-                <GridItem>
-                {image 
+          <Hidden smUp>
+            <div className={classes.title}>
+              {title}
+            </div>
+            <GridContainer justifyContent="flex-start">
+              <GridItem>
+                {image
                   ? <Avatar className={classes.avatar} variant="rounded" src={image} />
-                  : <Avatar className={classes.avatar} style={{"color": color}} variant="rounded" component={Icon} />
+                  : <Avatar className={classes.avatar} style={{ "color": color }} variant="rounded" component={Icon} />
                 }
-                </GridItem>
-                <GridItem xs>
-                  <div className={classes.title}>
-                    {title}
-                  </div>
-                  <div className={classes.description}>
-                    {description}
-                  </div>
-                </GridItem>
-              </GridContainer>
-            </GridItem>
-            <GridItem xs right>
-              {docs &&
-                <div className={classes.button}>
-                  <Button justIcon round color="teal"><SiReadthedocs /></Button>
+              </GridItem>
+              <GridItem xs right>
+                <div className={classes.description}>
+                  {description}
                 </div>
-              }
-            </GridItem>
-          </GridContainer>
+              </GridItem>
+            </GridContainer>
+            <div className={classes.spacer} />
+            <GridContainer justifyContent="space-between" alignItems="flex-end">
+              <GridItem xs left>
+                {listItems &&
+                  <List className={classes.list} dense>
+                    {listItems.map((item, pos) => {
+                      return (
+                        <ListItem key={`${item}-${pos}`}>
+                          <ListItemIcon>
+                            <ArrowRightIcon />
+                          </ListItemIcon>
+                          <ListItemText primary={item} />
+                        </ListItem>
+                      )
+                    })}
+                  </List>
+                }
+              </GridItem>
+              <GridItem>
+                {docs && <Button justIcon round color="teal"><SiReadthedocs /></Button>}
+              </GridItem>
+            </GridContainer>
+          </Hidden>
+          <Hidden xsDown>
+            <GridContainer>
+              <GridItem>
+                {image
+                  ? <Avatar className={classes.avatar} variant="rounded" src={image} />
+                  : <Avatar className={classes.avatar} style={{ "color": color }} variant="rounded" component={Icon} />
+                }
+              </GridItem>
+              <GridItem xs>
+                <div className={classes.title}>
+                  {title}
+                </div>
+                <div className={classes.description}>
+                  {description}
+                </div>
+              </GridItem>
+              <GridItem>
+                {docs && <Button justIcon round color="teal"><SiReadthedocs /></Button>}
+              </GridItem>
+              <GridItem xs={12}>
+                {listItems &&
+                  <List className={classes.list} dense>
+                    {listItems.map((item, pos) => {
+                      return (
+                        <ListItem key={`${item}-${pos}`}>
+                          <ListItemIcon>
+                            <ArrowRightIcon />
+                          </ListItemIcon>
+                          <ListItemText primary={item} />
+                        </ListItem>
+                      )
+                    })}
+                  </List>
+                }
+              </GridItem>
+            </GridContainer>
+          </Hidden>
         </Paper>
       </GridItem>
-    </GridContainer>
+    </GridContainer >
   );
 }
 
 TitlePanel.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  listItems: PropTypes.array,
   docs: PropTypes.bool,
   Icon: PropTypes.elementType,
   image: PropTypes.string,
