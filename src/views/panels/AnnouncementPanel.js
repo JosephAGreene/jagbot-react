098e3ddc1a34e143eb67d80909a0824ce247b2b6
Joseph Greene <josephadamgreene@gmail.com>
2021-10-03 22:01:30 -0400

    list layout conversion

--- src/views/panels/AnnouncementPanel.js
+++ src/views/panels/AnnouncementPanel.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from "prop-types";
 
 // Import API service
@@ -10,11 +9,24 @@ import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import IconButton from '@material-ui/core/IconButton';
+import Collapse from '@material-ui/core/Collapse';
+
+// Import custom views
+import EmbedPreviewPanel from './EmbedPreviewPanel';
 
 // Import custom components
 import GridContainer from '../../components/grid/GridContainer';
 import GridItem from '../../components/grid/GridItem';
-import Button from '../../components/buttons/Button';
+
+// Import icons
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 const styles = (theme) => ({
   menuRoot: {
@@ -24,50 +36,81 @@ const styles = (theme) => ({
     },
     '& .MuiButtonBase-root': {
       color: theme.palette.white.dark,
-      '&:hover' : {
+      '&:hover': {
         backgroundColor: theme.palette.gray.light,
       },
-      
+
     },
+  },
+  list: {
+    width: '100%',
+    backgroundColor: "inherit",
+    '& .MuiListItemText-root': {
+      color: theme.palette.white.main,
+      margin: 0,
+    },
+    '& .MuiTypography-colorTextSecondary': {
+      color: theme.palette.white.dark,
+    },
+    '& .MuiListItem-gutters': {
+      padding: 0,
+    },
+    '& .MuiListItemIcon-root': {
+      minWidth: "unset !important",
+      color: theme.palette.white.main,
+    },
+    '& .MuiTypography-displayBlock': {
+      display: "inline-block",
+      marginLeft: theme.spacing(1),
+    }
+  },
+  nested: {
+    marginLeft: theme.spacing(4),
+    [theme.breakpoints.down('sm')]: {
+      margin: 0,
+    },
+  },
+  expandMore: {
+    transform: 'rotate(-90deg)',
   },
   paper: {
     padding: "20px",
     marginBottom: theme.spacing(3),
     backgroundColor: theme.palette.gray.main,
   },
-  server: {
-    fontSize: 18,
-    letterSpacing: 0.5,
-    color: theme.palette.white.main,
+  buttonSpacer: {
+    marginLeft: theme.spacing(2),
   },
-  type: {
-    color: theme.palette.white.dark,
+  edit: {
+    color: theme.palette.purple.main,
+    '&:hover': {
+      color: theme.palette.purple.dark,
+    }
   },
-  buttons: {
-    marginTop: theme.spacing(1),
-    marginLeft: theme.spacing(1),
-    textDecoration: "none",
+  delete: {
+    color: theme.palette.error.main,
+    '&:hover': {
+      color: theme.palette.error.dark,
+    },
   },
 });
 
-function getPathname (moduleType) {
-  switch(moduleType) {
-    case "join":
-      return "/dashboard/develop/announcements/join";
-    case "optioned-response":
-      return "/dashboard/develop/announcements/leave";
-    case "random-response":
-      return "/dashboard/develop/announcements/banned";
-    default: 
-      return "/dashboard";
-  }
-}
-
 function AnnouncementPanel(props) {
-  const {classes, botId, module, setSelectedBot, setApiAlert} = props;
+  const { classes, botId, module, editModule, setSelectedBot, setApiAlert } = props;
   const [deleteAnchor, setDeleteAnchor] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
 
-  const pathname = getPathname(module.type);
+  const embedObject = {
+    title: module.embedTitle,
+    linkURL: module.embedLinkURL,
+    color: module.embedColor,
+    thumbnailURL: module.embedThumbnailURL,
+    mainImageURL: module.embedMainImageURL,
+    description: module.embedDescription,
+    fields: module.embedFields,
+    footer: module.embedFooter,
+    footerThumbnailURL: module.embedFooterThumbnailURL,
+  }
 
   const handleDeleteClick = (event) => {
     setDeleteAnchor(event.currentTarget);
@@ -101,31 +144,43 @@ function AnnouncementPanel(props) {
     <GridContainer>
       <GridItem xs>
         <Paper elevation={2} className={classes.paper} >
-          <GridContainer>
-            <GridItem xs={12} sm={12} md={8} lg={8}>
-              <div className={classes.server}>
-                Server: {module.responseChannel.serverName}
-              </div>
-              <div className={classes.type}>
-                Channel: {module.responseChannel.channelName}
-                Type: {module.type}
-              </div>
+          <GridContainer alignItems="flex-end">
+            <GridItem xs={12} sm={12} md={8} lg={9}>
+              <List className={classes.list}>
+                <ListItem>
+                  <ListItemText primary="Server:" secondary={module.responseChannel.serverName} />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Channel:" secondary={module.responseChannel.channelName} />
+                </ListItem>
+                <ListItem>
+                  <ListItemText primary="Type:" secondary={module.type} />
+                </ListItem>
+                <ListItem button onClick={() => setOpen(!open)}>
+                  <ListItemIcon>
+                    {open ? <ExpandMore /> : <ExpandMore className={classes.expandMore} />}
+                  </ListItemIcon>
+                  <ListItemText primary="Announcement" />
+                </ListItem>
+              </List>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List className={classes.list} component="div">
+                  <ListItem className={classes.nested}>
+                    {module.responseType === "basic"
+                      ? <ListItemText secondary={module.response} />
+                      : <EmbedPreviewPanel embedObject={embedObject} />
+                    }
+                  </ListItem>
+                </List>
+              </Collapse>
             </GridItem>
-            <GridItem xs={12} sm={12} md={4} lg={4} right>
-
-                <Link 
-                  className={classes.buttons}
-                  to={{
-                    pathname: pathname, 
-                    module: module,
-                  }} 
-                >
-                  <Button color="purple">Edit</Button>
-                </Link>
-
-              <div className={classes.buttons}>
-                <Button onClick={handleDeleteClick} color="danger">Delete</Button>
-              </div>
+            <GridItem xs={12} sm={12} md={4} lg={3} right>
+              <IconButton aria-label="edit" onClick={editModule}>
+                <EditIcon className={classes.edit} />
+              </IconButton>
+              <IconButton className={classes.buttonSpacer} aria-label="delete" onClick={handleDeleteClick} >
+                <DeleteIcon className={classes.delete} />
+              </IconButton>
               <Menu
                 id="delete-menu"
                 anchorEl={deleteAnchor}
@@ -143,7 +198,7 @@ function AnnouncementPanel(props) {
                 }}
               >
                 <MenuItem onClick={handleDeleteConfirm}>
-                    Confirm
+                  Confirm
                 </MenuItem>
                 <MenuItem onClick={handleDeleteClose}>Cancel</MenuItem>
               </Menu>
@@ -151,7 +206,7 @@ function AnnouncementPanel(props) {
           </GridContainer>
         </Paper>
       </GridItem>
-    </GridContainer>
+    </GridContainer >
   );
 }
 
