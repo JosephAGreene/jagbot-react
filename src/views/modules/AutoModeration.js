cba6f582bd481c89eeda5b537f5d4d85fc58955e
Joseph Greene <josephadamgreene@gmail.com>
2021-10-10 01:24:52 -0400

    scanModules now  autoModModules

--- src/views/modules/AutoModeration.js
+++ src/views/modules/AutoModeration.js
@@ -31,32 +31,32 @@ function AutoModeration(props) {
       <GridContainer>
         <AutoModerationPanel
           title="Auto Roles"
-          description="Automatically assign roles to members when they first join your server"
-          module={selectedBot.scanModules.find(module => module.type === "auto-role")}
+          description="Automatically assign roles to members when they first join your server."
+          module={selectedBot.autoModModules.find(module => module.type === "auto-role")}
           image={autoroleImage}
         />
         <AutoModerationPanel
           title="Banned Words"
           description="Create of a list of banned words. Tell your bot what to do when someone posts those words."
-          module={selectedBot.scanModules.find(module => module.type === "word-filter")}
+          module={selectedBot.autoModModules.find(module => module.type === "word-filter")}
           image={bannedwordsImage}
         />
         <AutoModerationPanel
           title="Discord Invite Links"
           description="Have your bot take action when someone posts discord invite links in your server."
-          module={selectedBot.scanModules.find(module => module.type === "invite-filter")}
+          module={selectedBot.autoModModules.find(module => module.type === "invite-filter")}
           image={invitelinksImage}
         />
         <AutoModerationPanel
           title="Mass Caps"
           description="Have a member with an infatuation for the capslock key? Give them motivation to change."
-          module={selectedBot.scanModules.find(module => module.type === "masscaps-filter")}
+          module={selectedBot.autoModModules.find(module => module.type === "masscaps-filter")}
           image={masscapsImage}
         />
         <AutoModerationPanel
           title="Mass Mentions"
           description="Take action when a member has gone overboard with the mentions. You decide what overboard is."
-          module={selectedBot.scanModules.find(module => module.type === "massmentions-filter")}
+          module={selectedBot.autoModModules.find(module => module.type === "massmentions-filter")}
           image={massmentionsImage}
         />
       </GridContainer>
