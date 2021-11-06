7d68c46efef01d91d5aaf5a59eb5d717ab0b28f5
Joseph Greene <josephadamgreene@gmail.com>
2021-11-05 20:11:50 -0400

    proptypes

--- src/views/settings/Settings.js
+++ src/views/settings/Settings.js
@@ -174,6 +174,13 @@ function UpdateName(props) {
   )
 }
 
+UpdateName.propTypes = {
+  botName: PropTypes.string.isRequired,
+  botId: PropTypes.string.isRequired,
+  setSelectedBot: PropTypes.func.isRequired,
+  setApiAlert: PropTypes.func.isRequired,
+};
+
 function UpdatePrefix(props) {
   const { botPrefix, botId, setSelectedBot, setApiAlert } = props;
   const classes = updateStyles();
@@ -287,8 +294,15 @@ function UpdatePrefix(props) {
   )
 }
 
+UpdatePrefix.propTypes = {
+  botPrefix: PropTypes.string.isRequired,
+  botId: PropTypes.string.isRequired,
+  setSelectedBot: PropTypes.func.isRequired,
+  setApiAlert: PropTypes.func.isRequired,
+};
+
 function UpdateToken(props) {
-  const  { botToken, botId, setSelectedBot, setApiAlert } = props;
+  const { botToken, botId, setSelectedBot, setApiAlert } = props;
   const classes = updateStyles();
 
   const { register, handleSubmit, watch, reset, setError, formState: { errors } } = useForm({
@@ -325,7 +339,7 @@ function UpdateToken(props) {
         severity: "success",
         message: "Bot token has been updated!"
       });
-    }  else if (res.status === 418) {
+    } else if (res.status === 418) {
       setError("token", { type: "manual", message: res.data });
     } else if (res.status === "dead") {
       setApiAlert({
@@ -413,6 +427,152 @@ function UpdateToken(props) {
   )
 }
 
+UpdateToken.propTypes = {
+  botToken: PropTypes.string.isRequired,
+  botId: PropTypes.string.isRequired,
+  setSelectedBot: PropTypes.func.isRequired,
+  setApiAlert: PropTypes.func.isRequired,
+};
+
+function UpdateStatus(props) {
+  const { botStatusType, botStatusName, botId, setSelectedBot, setApiAlert } = props;
+  const classes = updateStyles();
+
+  const { register, handleSubmit, watch, reset, setError, formState: { errors } } = useForm({
+    resolver: joiResolver(
+      Joi.object({
+        statusType: Joi.string().trim().valid('none', 'playing', 'streaming', 'listening', 'watching', 'competing').required()
+          .messages({
+            "string.empty": 'Activity type is required ("None" is valid)',
+            "any.required": 'Activity type is required ("None" is valid)',
+          }),
+        statusName: Joi.string().trim().max(30),
+      })
+    ),
+    defaultValues: {
+      statusType: botStatusType ? botStatusType : "none",
+      statusName: botStatusName ? botStatusName : "",
+    },
+  });
+
+  const currentStatusType = watch("statusType");
+  const currentStatusName = watch("statusName");
+  const edited = isDifferent(botStatusType, currentStatusType) || isDifferent(botStatusName, currentStatusName);
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    // const payload = {
+    //   botId: botId,
+    //   token: data.token,
+    // }
+    // const res = await BotService.updateBotToken(payload);
+
+    // if (res.status === 200) {
+    //   setSelectedBot(res.data);
+    //   setApiAlert({
+    //     status: true,
+    //     duration: 2500,
+    //     severity: "success",
+    //     message: "Bot token has been updated!"
+    //   });
+    // }  else if (res.status === 418) {
+    //   setError("token", { type: "manual", message: res.data });
+    // } else if (res.status === "dead") {
+    //   setApiAlert({
+    //     status: true,
+    //     duration: 2500,
+    //     severity: "error",
+    //     message: "Server is busy or offline. Try again later."
+    //   });
+    // } else {
+    //   console.log(res.data);
+    //   setApiAlert({
+    //     status: true,
+    //     duration: 2500,
+    //     severity: "error",
+    //     message: "Something went wrong. Hint: Check the console!"
+    //   });
+    // }
+  }
+
+  const resetField = () => {
+    reset();
+  }
+
+  return (
+    <Paper elevation={2} className={classes.paper} >
+      <form autoComplete="off" onSubmit={handleSubmit(onSubmit)} >
+        <GridContainer justifyContent="flex-end" alignItems="center">
+          <GridItem xs={12}>
+            <OutlinedInput
+              labelText="Activity"
+              description="Your bot's activity text"
+              id="statusName"
+              name="statusName"
+              formControlProps={{ fullWidth: true }}
+              inputProps={{ ...register("statusName"), maxLength: 30 }}
+              error={errors}
+              labelProps={{ shrink: true }}
+            />
+            <OutlinedInput
+              labelText="Type"
+              description="Your bot's activity type"
+              id="statusType"
+              name="statusType"
+              formControlProps={{ fullWidth: true }}
+              inputProps={{ ...register("statusType"), maxLength: 30 }}
+              error={errors}
+              labelProps={{ shrink: true }}
+            />
+          </GridItem>
+          <GridItem right>
+            {edited
+              ?
+              <>
+                <Button
+                  onClick={resetField}
+                  variant="contained"
+                  color="orange"
+                  className={classes.button}
+                >
+                  Reset
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="purple"
+                  className={classes.button}
+                >
+                  Update
+                </Button>
+              </>
+              :
+              <>
+                <Button
+                  variant="contained"
+                  color="gray"
+                  disabled
+                  className={classes.button}
+                >
+                  Reset
+                </Button>
+                <Button
+                  variant="contained"
+                  color="gray"
+                  disabled
+                  className={classes.button}
+                >
+                  Update
+                </Button>
+              </>
+            }
+          </GridItem>
+        </GridContainer>
+      </form>
+    </Paper >
+  )
+}
+
 function Settings(props) {
   const { selectedBot, setSelectedBot, setApiAlert } = props;
 
@@ -438,6 +598,13 @@ function Settings(props) {
       />
       <UpdateToken
         botToken={selectedBot.botToken}
+        botId={selectedBot._id}
+        setSelectedBot={setSelectedBot}
+        setApiAlert={setApiAlert}
+      />
+      <UpdateStatus
+        botStatusType={selectedBot.botStatusType ? selectedBot.botStatusType : ""}
+        botStatusName={selectedBot.botStatusName ? selectedBot.botStatusName: ""}
         botId={selectedBot._id}
         setSelectedBot={setSelectedBot}
         setApiAlert={setApiAlert}
