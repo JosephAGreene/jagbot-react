facabb5e7ce5a133c3fa5f05fb601aca906f2213
Joseph Greene <josephadamgreene@gmail.com>
2022-01-15 18:42:13 -0500

    rought draft doc alert

---
+++ src/components/info/DocAlert.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+export default function DocAlert(props) {
+  const {children} = props;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card>
+
+      <CardActions disableSpacing>
+        <IconButton
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          {children}
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
