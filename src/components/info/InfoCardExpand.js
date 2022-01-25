25f0013eee27e09a72e02fc6ccf0a8fece11b62b
Joseph Greene <josephadamgreene@gmail.com>
2022-01-25 01:23:36 -0500

    option border props & accompanying styles

--- src/components/info/InfoCardExpand.js
+++ src/components/info/InfoCardExpand.js
@@ -49,26 +49,49 @@ const styles = (theme) => ({
   primary: {
     color: theme.palette.primary.main,
   },
+  borderprimary: {
+    borderLeft: `5px solid ${theme.palette.primary.main}`,
+  },
   secondary: {
     color: theme.palette.secondary.main,
+  },
+  bordersecondary: {
+    borderLeft: `5px solid ${theme.palette.secondary.main}`,
   },
   danger: {
     color: theme.palette.error.main,
   },
+  borderdanger: {
+    borderLeft: `5px solid ${theme.palette.error.main}`,
+  },
   purple: {
     color: theme.palette.purple.dark,
+  },
+  borderpurple: {
+    borderLeft: `5px solid ${theme.palette.purple.dark}`,
   },
   teal: {
     color: theme.palette.teal.dark,
   },
+  borderteal: {
+    borderLeft: `5px solid ${theme.palette.teal.dark}`,
+  },
   orange: {
     color: theme.palette.orange.dark,
   },
+  borderorange: {
+    borderLeft: `5px solid ${theme.palette.orange.dark}`,
+  }
 });
 
 function InfoCardExpand(props) {
-  const { classes, color, icon, title, subheader, content } = props;
+  const { classes, border, color, icon, title, subheader, content } = props;
   const [expanded, setExpanded] = React.useState(false);
+
+  const rootClasses = clsx({
+    [classes.root]: true,
+    [classes[`border${color}`]]: border && color,
+  });
 
   const iconClasses = clsx({
     [classes.icon]: true,
@@ -80,7 +103,7 @@ function InfoCardExpand(props) {
   };
 
   return (
-    <Card className={classes.root}>
+    <Card className={rootClasses}>
       <CardHeader
         className={classes.header}
         onClick={handleExpandClick}
@@ -116,6 +139,7 @@ function InfoCardExpand(props) {
 
 InfoCardExpand.propTypes = {
   classes: PropTypes.object.isRequired,
+  border: PropTypes.bool,
   color: PropTypes.string,
   icon: PropTypes.object,
   title: PropTypes.string.isRequired,
